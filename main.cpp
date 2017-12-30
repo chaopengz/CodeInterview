5a09d6459779eec3613871c54e611c8edd2104ea
cpz <cpz@pku.edu.cn>
2017-12-30 11:01:11 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-#include "Fibonacci.cpp"
+//#include "Fibo nacci.cpp"
+#include "head.h"
+
+bool find(vector<int> nums, int target);
 
 int main()
 {
-    Solution so;
-    int a[] = {2, 1};
-    vector<int> v(a, a + 2);
-    cout << so.Fibonacci(39);
+    int a[] = {1, 2, 3, 5, 6, 7, 8};
+    vector<int> v(a, a + 7);
+    cout << find(v, 8);
+}
+
+bool find(vector<int> nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    int mid;
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+            return true;
+        else if (nums[mid] > target)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
 }
