708cf1b87a56cd4607a95a1346e940ce0d2183f4
cpz <cpz@pku.edu.cn>
2018-01-05 13:38:41 +0800

    update

--- minNumberInRotateArray.cpp
+++ minNumberInRotateArray.cpp
@@ -5,40 +5,31 @@
 
 class Solution {
 public:
-    int minNumberInRotateArray(vector<int> rotateArray)
+    int minNumberInRotateArray(vector<int> array)
     {
-        if (!rotateArray.size())
-            return 0;
-        if (rotateArray.size() == 1)
-            return rotateArray[0];
-        int len = rotateArray.size();
-
-        if (rotateArray[0] < rotateArray[len - 1])//没有旋转
-            return rotateArray[0];
-
-        int low = 0, high = len - 1;
+        int low = 0;
+        int high = array.size() - 1;
         int mid;
-        while (low <= high)
+        while (low < high)
         {
-            mid = (low + high) / 2;
-            if (isFind(rotateArray, mid))
-                return rotateArray[mid];
-            else if (rotateArray[mid] >= rotateArray[0])
+            mid = low + (high - low) / 2;
+            if (array[mid] > array[high])//最小值一定在mid右边
+            {
                 low = mid + 1;
-            else
-                high = mid - 1;
+            } else if (array[mid] == array[high])//最小值有可能在mid左边,也有可能在右边
+            {
+                high = high - 1;//去掉重复的
+            } else//array[mid]<array[high] => mid右边是单调,mid是右边最小得数,也可能是左边最小的数,所以high = mid,而非high = mid-1
+            {
+                high = mid;
+            }
         }
-
-        return 0;
-    }
-
-    bool isFind(vector<int> nums, int index)
-    {
-        if (index == nums.size() - 1)
-            return nums[index] < nums[index - 1];
-        else if (index == 0)
-            return nums[index] < nums[nums.size() - 1];
-        else
-            return nums[index] < nums[index + 1] && nums[index] < nums[index - 1];
+        return array[low];
+//        int minNum = INT32_MAX;
+//        for (auto i:array)
+//        {
+//            minNum = min(minNum, i);
+//        }
+//        return minNum;
     }
 };
