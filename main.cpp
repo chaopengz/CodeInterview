c597ac2dfa6a3d3e9e6d2c27e065a7b9cbfed248
cpz <cpz@pku.edu.cn>
2018-01-07 00:25:11 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
-#include "Mirror.cpp"
+#include "printMatrix.cpp"
 #include "buildTree.h"
 
 int main()
 {
-    BuildTree buildTree;
-    TreeNode *root1 = buildTree.stringToTree("[8,6,10,5,7,9,11]");
+    int a[] = {1, 2, 3, 4};
+    int b[] = {5, 6, 7, 8};
+    int c[] = {9, 10, 11, 12};
+    int d[] = {13, 14, 15, 16};
+    vector<int> va(a, a + 4);
+    vector<int> vb(b, b + 4);
+    vector<int> vc(c, c + 4);
+    vector<int> vd(d, d + 4);
+    vector<vector<int>> vv;
+    vv.push_back(va);
+    vv.push_back(vb);
+    vv.push_back(vc);
+    //vv.push_back(vd);
     Solution solution;
-    solution.Mirror(root1);
-
+    vector<int> ans;
+    ans = solution.printMatrix(vv);
+    for (auto i:ans)
+        cout << i << " ";
 }
 
--- printMatrix.cpp
+++ printMatrix.cpp
@@ -8,12 +8,38 @@ class Solution {
 public:
     vector<int> printMatrix(vector<vector<int>> matrix)
     {
-
+        int m = matrix.size();
+        int n = matrix[0].size();
+        printOneLoop(matrix, 0, 0, m - 1, n - 1);
+        return ans;
     }
 
     void printOneLoop(vector<vector<int>> matrix, int i, int j, int m, int n)
     {
+        int rows = m - i;
+        int cols = n - j;
 
+        for (int k = j; k <= n; ++k)
+        {
+            ans.push_back(matrix[i][k]);
+        }
+        for (int l = i + 1; l <= m; ++l)
+        {
+            ans.push_back(matrix[l][n]);
+        }
+        for (int i1 = n - 1; i1 >= j; --i1)//
+        {
+            if (m != i)
+                ans.push_back(matrix[m][i1]);
+        }
+        for (int k1 = m - 1; k1 > i; --k1)
+        {
+            if (n != j)
+                ans.push_back(matrix[k1][j]);
+        }
+        if (rows > 1 && cols > 1)
+            printOneLoop(matrix, i + 1, j + 1, m - 1, n - 1);
     }
+
     vector<int> ans;
 };
