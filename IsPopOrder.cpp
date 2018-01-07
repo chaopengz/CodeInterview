0e3e5faf1adcf74977caa4b98508892df8f27881
chaopengz <cpz@pku.edu.cn>
2018-01-07 17:22:58 +0800

    update

--- IsPopOrder.cpp
+++ IsPopOrder.cpp
@@ -7,6 +7,24 @@ class Solution {
 public:
     bool IsPopOrder(vector<int> pushV, vector<int> popV)
     {
-
+        int len = pushV.size();
+        int i = 0, j = 0;
+        while (j < len || i < len)
+        {
+            if (st.empty())
+            {
+                st.push(pushV[i++]);
+            } else if (popV[j] == st.top())
+            {
+                st.pop();
+                j++;
+            } else
+            {
+                st.push(pushV[i++]);
+            }
+        }
+        return st.empty();
     }
+
+    stack<int> st;
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-#include "MinStack.cpp"
-#include "buildTree.h"
+#include "IsPopOrder.cpp"
 
 
 int main()
 {
     Solution solution;
-    vector<int> ans;
+    int a[] = {1, 2, 3, 4, 5};
+    int b[] = {4, 5, 3, 2, 1};
+    int c[] = {4, 3, 5, 1, 2};
+    vector<int> va(a, a + 5);
+    vector<int> vb(b, b + 5);
+    vector<int> vc(c, c + 5);
+    cout << solution.IsPopOrder(va, vc);
 
 }
 
