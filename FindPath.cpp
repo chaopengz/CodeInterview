08375a2cc2fc1e42b97035f2010a66f8ee3d2dcb
cpz <cpz@pku.edu.cn>
2018-01-08 00:21:24 +0800

    update

--- FindPath.cpp
+++ FindPath.cpp
@@ -5,30 +5,31 @@
 
 class Solution {
 public:
-    vector<vector<int>> FindPath(TreeNode *root, int expectNumber)
+    vector<vector<int>> FindPath(TreeNode *root, int sum)
     {
         vector<int> v;
-        FindPathRec(root, expectNumber, v);
+        FindPathRec(root, sum, v);
         return ans;
     }
 
-    void FindPathRec(TreeNode *root, int exprectNumbers, vector<int> v)
+    void FindPathRec(TreeNode *root, int sum, vector<int> v)
     {
-        if (!root->left && !root->right)//leaf node
+        if (!root)
+            return;
+        if (root->left == nullptr && root->right == nullptr)//leaf node
         {
-            if (exprectNumbers == root->val)
+            if (root->val == sum)
             {
                 v.push_back(root->val);
                 ans.push_back(v);
             }
             return;
-        } else
-        {
-            v.push_back(root->val);
-            FindPathRec(root->left, exprectNumbers - root->val, v);
-            FindPathRec(root->right, exprectNumbers - root->val, v);
         }
+        v.push_back(root->val);
+        FindPathRec(root->left, sum - root->val, v);
+        FindPathRec(root->right, sum - root->val, v);
     }
 
     vector<vector<int>> ans;
+
 };
