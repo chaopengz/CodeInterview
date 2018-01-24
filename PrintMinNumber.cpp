5c46e674091d506b42a1b7a9437193c8a4e5b6ce
chaopengz <cpz@pku.edu.cn>
2018-01-24 11:35:04 +0800

    update

--- PrintMinNumber.cpp
+++ PrintMinNumber.cpp
@@ -7,6 +7,24 @@ class Solution {
 public:
     string PrintMinNumber(vector<int> numbers)
     {
+        vector<string> strs;
+        string ans = "";
+        for (auto i:numbers)
+        {
+            strs.push_back(to_string(i));
+        }
+        sort(strs.begin(), strs.end(), compare);
+        for (auto i:strs)
+            ans += i;
+        return ans;
+    }
 
+    static bool compare(string str1, string str2)
+    {
+        string s1 = str1 + str2, s2 = str2 + str1;
+        if (s1 < s2)
+            return true;
+        else
+            return false;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,42 +1,16 @@
 #include <iostream>
-#include "NumberOf1Between1AndN_Solution.cpp"
+#include "PrintMinNumber.cpp"
+
 //#include "buildTree.h"
-
-int countOne(int n)
-{
-    int ans = 0;
-    while (n)
-    {
-        if (n % 10 == 1)
-            ans++;
-        n = n / 10;
-    }
-    return ans;
-}
-
-int countOneUnitN(int n)
-{
-    int sum = 0;
-    for (int i = 1; i <= n; ++i)
-    {
-        sum += countOne(i);
-    }
-    return sum;
-}
-
-
+//
 int main()
 {
     Solution solution;
-    for (int i = 1; i < INT32_MAX; ++i)
-    {
-        int ans1, ans2;
-        ans1 = solution.NumberOf1Between1AndN_Solution(i);
-        ans2 = countOneUnitN(i);
-        if (ans1 != ans2)
-            cout << i << " " << ans1 << " " << ans2 << endl;
-        else
-            cout << i << " " << ans1 << " " << ans2 << endl;
-    }
+    int a[] = {3, 32, 321};
+//    int a[] = {3334,3,3333332};
+    vector<int> v(a, a + 3);
+    cout << solution.PrintMinNumber(v);
 }
+
+
 
