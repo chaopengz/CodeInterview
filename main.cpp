6ced1ed9eafea25ad5c1bd747d2adb259589b1c6
chaopengz <cpz@pku.edu.cn>
2018-01-25 00:58:38 +0800

    mid travel

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     int a[] = {1, 2, 3, 4, 5, 6, 10, 21, 30, 50};
     vector<int> v(a, a + 10);
     BuildTree buildTree;
-    TreeNode *root = buildTree.stringToTree("[]");
+    TreeNode *root = buildTree.stringToTree("[5,3,7,2,4,6,8]");
     TreeNode *node = solution.KthNode(root, 1);
     cout << node->val;
 }
