c32058e6c872e867a5bb0729e3c03f930e41bc31
cpz <cpz@pku.edu.cn>
2018-01-25 11:15:08 +0800

    update

--- LastRemaining_Solution.cpp
+++ LastRemaining_Solution.cpp
@@ -1,13 +1,49 @@
 //
 // Created by cpz on 2018/1/24.
 //
-#include "head.h"
+//#include "head.h"
 
 class Solution {
 public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode(int x) : val(x), next(nullptr) {};
+    };
+
     int LastRemaining_Solution(int n, int m)
     {
+        if (n <= 1)
+            return 0;
+        ListNode *head = new ListNode(0);
+        ListNode *pre = head, *node = nullptr;
+        for (int i = 1; i < n; ++i)
+        {
+            node = new ListNode(i);
+            pre->next = node;
+            pre = node;
+        }
+        if (node)
+            node->next = head;
 
+        ListNode *currentNode = head;
+
+        for (int j = 1; j < n; ++j)
+        {
+            for (int i = 1; i < m; ++i)
+            {
+                pre = currentNode;
+                currentNode = currentNode->next;
+            }
+            //deleteCurrentNode
+            pre->next = currentNode->next;
+            currentNode = pre->next;
+        }
+
+        if (currentNode)
+            return currentNode->val;
     }
+
 };
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "KthNode.cpp"
+#include "LastRemaining_Solution.cpp"
 
 #include "buildTree.h"
 
@@ -7,13 +7,7 @@
 int main()
 {
     Solution solution;
-    int a[] = {1, 2, 3, 4, 5, 6, 10, 21, 30, 50};
-    vector<int> v(a, a + 10);
-    BuildTree buildTree;
-    TreeNode *root = buildTree.stringToTree("[5,3,7,2,4,6,8]");
-    TreeNode *node = solution.KthNode(root, 8);
-    if (node)
-        cout << node->val;
+    cout << solution.LastRemaining_Solution(5, 3);
 }
 
 
